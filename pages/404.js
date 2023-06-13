644b4c3abc4d75c6ffdb5b1d8a00bd15ad2c81a0
enmohamedMallahi <mohaxxmall@ygmail.com>
2023-06-13 13:11:42 +0000

    update the UI

--- pages/404.js
+++ pages/404.js
@@ -1,42 +1,32 @@
 import Link from 'next/link';
-import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
 const NotFound = () => {
 	return (
 		<div>
-			<Header />
-			<div class='max-w-screen-lg px-4 md:px-8 mx-auto'>
-				<div class='grid sm:grid-cols-2 gap-8'>
-					<div class='flex flex-col justify-center items-center sm:items-start md:py-24 lg:py-32'>
-						<p class='text-indigo-500 text-sm md:text-base font-semibold uppercase mb-4'>
-							Error 404
-						</p>
-						<h1 class='text-gray-800 text-2xl md:text-3xl font-bold text-center sm:text-left mb-2'>
-							Page not found
-						</h1>
-
-						<p class='text-gray-500 md:text-lg text-center sm:text-left mb-8'>
-							The page you&apos;re looking for doesn&apos;t exist.
-						</p>
-
-						<Link
-							href='/'
-							class='inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 py-2'
+			<Navbar />
+			<main className='grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8'>
+				<div className='text-center'>
+					<p className='text-base font-semibold text-indigo-600'>404</p>
+					<h1 className='mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
+						Page not found
+					</h1>
+					<p className='mt-6 text-base leading-7 text-gray-600'>
+						Sorry, we couldn&apos;t find the page you&apos;re looking for.
+					</p>
+					<div className='mt-10 flex items-center justify-center gap-x-6'>
+						<a
+							href='#'
+							className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
 						>
-							Go home
-						</Link>
-					</div>
-
-					<div class='h-80 md:h-auto bg-gray-100 overflow-hidden shadow-lg rounded-lg relative'>
-						<img
-							src='https://images.unsplash.com/photo-1590642916589-592bca10dfbf?auto=format&q=75&fit=crop&w=600'
-							loading='lazy'
-							alt='Photo by @heydevn'
-							class='w-full h-full object-cover object-center absolute inset-0'
-						/>
+							Go back home
+						</a>
+						<a href='#' className='text-sm font-semibold text-gray-900'>
+							Contact support <span aria-hidden='true'>&rarr;</span>
+						</a>
 					</div>
 				</div>
-			</div>
+			</main>
 		</div>
 	);
 };
